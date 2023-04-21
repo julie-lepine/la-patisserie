4ecf6ef7af2e8c48ade82cebc18c205de7fc9e26
julie-lepine <lepine.julie@live.fr>
2023-04-21 15:25:34 +0200

    refacto

---
+++ assets/js/devis.js
@@ -0,0 +1,6 @@
+///////// DEVIS PAGE - ALERT ON THE SUBMIT BTN
+const submitBtn = document.getElementById('submitBtn');
+
+submitBtn.addEventListener('click', function() {
+    alert('Merci, votre message a bien été envoyé.')
+})
